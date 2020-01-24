388b7503e18efcf5a1220f01e514fcb410dce5d0
HAKAN KELES <40118057+fortisfortunaadiuvat@users.noreply.github.com>
2020-01-24 17:25:42 +0300

    Add files via upload

---
+++ AbstractClassnInterface/AbstractClassnInterface/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AbstractClassnInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape[] shapes = { new Circle(5), new Rectangle(4, 5) };
+            
+            foreach(Shape s in shapes)
+            {
+                s.GetInfo();
+
+                Console.WriteLine($"{s.Name} area: {s.Area():f2}");
+            }
+        }
+    }
+}
